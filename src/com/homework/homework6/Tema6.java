7e72d2906c14222a7d46513dca5714aa7c64bbce
Ioan Lefter <lefter_ioan@yahoo.com>
2020-09-20 22:21:56 +0300

    Tema 6 completa

---
+++ src/com/homework/homework6/Tema6.java
@@ -0,0 +1,32 @@
+package com.homework.homework6;
+
+public class Tema6 {
+    public static void main(String[] args) {
+        Wall wall = new Wall(5, 4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
+
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1, 1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+
+    }
+}
