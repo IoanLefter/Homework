f974151601882ce28bec47305cf3955ef10ae2b0
IoanLefter <lefter_ioan@yahoo.com>
2020-09-30 19:39:00 +0300

    Tema 7 completa

---
+++ src/com/homework/homework7/Rectangle.java
@@ -0,0 +1,39 @@
+package com.homework.homework7;
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if (length < 0) {
+            this.length = 0;
+        } else {
+            this.length = length;
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+}
---
+++ src/com/homework/homework7/Tema7.java
@@ -0,0 +1,33 @@
+package com.homework.homework7;
+
+public class Tema7 {
+    public static void main(String[] args) {
+        Vehicle vehicul1 = new Vehicle();
+        vehicul1.moving(22);
+
+        Car masina1 = new Car(200, 150, 400, true, "black", 5, "electric");
+        masina1.setNumberOfDoors(0);
+        masina1.startEngine();
+
+        vehicul1.steering("left");
+
+        Mercedes benzC = new Mercedes(220, 170, 500, true, "black", 5, "hybrid", "benz C");
+        benzC.startEngine();
+
+
+        System.out.println("Ex 2------------------------------");
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5, 10, 5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
+
+
+    }
+
+}
